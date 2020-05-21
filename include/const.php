803292e8381825a54493d381681ef679602dbd5e
EC2 Default User <ec2-user@ip-172-31-93-138.ec2.internal>
2020-05-21 17:56:46 +0900

    issue/24で検索結果を作成しました。constのDBハンドルを更新しました。

--- include/const.php
+++ include/const.php
@@ -3,9 +3,9 @@
 define('TAX', 1.05);  // 消費税
  
 define('DB_HOST',   'localhost'); // データベースのホスト名又はIPアドレス
-define('DB_USER',   '');  // MySQLのユーザ名
-define('DB_PASSWD', '');    // MySQLのパスワード
-define('DB_NAME',   '');    // データベース名
+define('DB_USER',   getenv('DB_USER'));  // MySQLのユーザ名
+define('DB_PASSWD', getenv('DB_USER'));    // MySQLのパスワード
+define('DB_NAME',   getenv('DB_USER'));    // データベース名
  
 define('HTML_CHARACTER_SET', 'UTF-8');  // HTML文字エンコーディング
 define('DB_CHARACTER_SET',   'UTF8');   // DB文字エンコーディング
