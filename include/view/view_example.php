0562b7e27a3a9b882d16e710c909ed7c57f57f1c
FU XIANG <fuxiang0931@gmail.com>
2020-05-21 11:01:03 +0900

    プロジェクトアップロード

---
+++ include/view/view_example.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="UTF-8">
+        <title>view example</title>
+        <link rel="stylesheet" href="../css/main_style.css" type="text/css" />
+    </head>
+    <body>
+        <header>
+            <div class="header_box">
+                <a href="">
+                    <img class="logo" src="../web_image/logo.png"></img>
+                </a>
+                <div class="header_nemu"><a class="header_link" href="">Menu</a></div>
+                <div class="header_nemu header_spe"><a class="header_link" href="">気になる</a></div>
+                <div class="header_nemu"><a class="header_link" href="">TOP</a></div>
+            </div>
+        </header>
+        <section class="content">
+            <!--ここに追加-->
+            <img src="../web_image/test.png"></img>
+        </section>
+    </body>
+    <footer>
+        <section class="foot_all">
+            <p style="text-align:center"><a class="foot_link" href="question.php">お問い合わせ</a></p>
+            <p class="footer_img"><img src="../web_image/team_logo.png"></img></p>
+            <p style="text-align:center;color:black;font-size:10px"><small>Copyright &copy; B-Team All Rights Reserved.</small></p>
+        </section>
+    </footer>
+</html>
