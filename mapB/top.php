691db1b9b58971e3d929c95ac8ef18f42a6ababd
EC2 Default User <ec2-user@ip-172-31-88-229.ec2.internal>
2020-06-22 12:02:28 +0900

    #2001にフォルダ追加

---
+++ mapB/top.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lamg="ja">
+    <head>
+        <meta charset="UTF-8">
+        <title>トップページ</title>
+       <link rel="stylesheet" href="./main_style.css" type="text/css" />
+        <style>
+            .center{
+                text-align: center;
+            }
+            .font-size{
+                font-size: 150%;
+                text-decoration: underline;
+            }
+        </style>
+    </head>
+    <body>
+         <header>
+            <div class="header_box">
+                <a href="search.php">
+                    <img class="logo" src="../web_image/logo.png"></img>
+                </a>
+                <div class="header_menu">
+                <a class="header_link" href="login.php">ログイン</a>
+                </div>
+                <div class="header_menu header_spe"><a class="header_link" href="like.php">ルート探索</a></div>
+                <div class="header_menu header_spe"><a class="header_link" href="main.php">聖地一覧</a></div>
+            </div>
+        </header>
+        <div class="center"><h1><a href="guide.php">初めての方はこちら</a></h1></div>
+        
+        <h1 class="center">
+            <img class="kaeru" src="kaeru.png">聖地巡礼のマナー
+            <img src="usagi.png">
+        </h1>
+        
+        <h2 class="center">★聖地は二次元ではなく現実です</h2>
+        <p class="center">聖地は歴史的に重要な建物から個人宅まで様々な場所があります。建物の破壊行為やゴミのポイ捨てなどはNGです！</p>
+        <p class="center">また聖地のほとんどは、聖地巡礼の為に用意された場所ではありません。そこで働く人、住んでいる人がいることを十分理解し、</p>
+        <p class="center">現地の方々に不信感を与えないようにしていきましょう。<br>
+        
+        <h2 class="center">★写真撮影について</h2>
+        <p class="center">SNS等で聖地の写真をたくさん見かけるようになりましたが、写真撮影NGの場所も少なくありません。</p>
+        <p class="center">特に公共施設や個人宅などの写真や位置情報の取扱いには十分注意してください。</p>
+        
+        <h2 class="center">★街やアニメに感謝の気持ちを忘れずに</h2>
+        <p class="center">確かに観光や協賛商店等では、アニメの舞台に取り上げられることで経済が活性化するのは事実ですが、</p>
+        <p class="center">迎え入れる土地の人々もおもてなしをしてくれている対等な関係であることを忘れてはいけません。</p>
+        <div class="center"><p><img src="kaeru1.png"></p></div>
+        <h1 class="center">ご協力お願いします。</h1>
+        <footer>
+            <section class="foot_all">
+                <p style="text-align:center"><a class="foot_link" href="question.php">お問い合わせ</a></p>
+                <p class="footer_img"><img src="../web_image/team_logo.png"></img></p>
+                <p style="text-align:center;color:black;font-size:10px"><small>Copyright &copy; B-Team All Rights Reserved.</small></p>
+            </section>
+        </footer>
+    </body>
+</html>
